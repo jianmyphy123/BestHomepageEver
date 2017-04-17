9b431671aa4b27b2c73b15a8e730f1b06a10fcf0
jianmyphy123 <jianmyphy123@tutanota.com>
2017-04-17 18:55:50 +0200

    Initial Commit

---
+++ about/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta name="author" content="Justin Carver">
+<title>About BestHomepageEver | Hisory, Mission Statement, and Future Goals</title>
+<meta name="description" content="Brief history of the homepage, my personal mission statement, and future goals going forward for all things related to the best homepage on the Internet.">
+<!-- Bootstrap 3.1.1 -->
+
+<link href="../bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<link href="../bootstrap/3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+<!-- CSS -->
+<link href="../news/news.css" rel="stylesheet" />
+
+<!-- Google Analytics Begin -->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31333288-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+
+
+</head>
+
+<body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item" href="../"><img src="../Logo/square_small.jpg" /> Go to Homepage</a>
+          <a class="blog-nav-item" href="../news/" title="Homepage News & Updates">News and Updates</a>                   
+          <a class="blog-nav-item" href="../faq/" title="Questions and Answers">Questions and Help</a> 
+          <a class="blog-nav-item active" href="#">About the homepage</a>
+          <a class="blog-nav-item" href="../tips/" title="Tips and Tricks">Tips and Tricks</a>          
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title">History and Goals of the homepage</h1>
+        <p class="lead blog-description"></p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main"> 
+        
+            <h2>About BestHomepageEver</h2>
+                <p>My name is Justin Carver, and this site began as a school project at the <a href="http://uoregon.edu">University of Oregon</a> (USA), in my very first computer class in 1999. The idea was to start a page that had links to all of the most commonly used sites for myself, friends, and family.</p>
+                <p>As time progressed, I began seeing friends of friends using it at the University, so over time my goal was to continue to add sites that were relevant and useful for people to use in their daily lives.</p>            
+                <p>Now, with a widespread national and new international userbase, I'm very proud and thankful to say that BHE is a useful tool for the world to enjoy.</p><p>Thanks for your support!</p>        
+                <hr />
+            <h2>Mission & Goals</h2>    
+           	 <p>My goal has always been, and will continue to be, the following:</p>
+                <ul>                    
+                    <li>Usefulness</li>
+                    <li>Simplicity</li>
+                    <li>Speed</li>
+                    <li>No Advertisements</li>
+                </ul>
+            <hr />
+            <h3><img src="../pictures/about/check.png" /> Usefulness</h3><p>With each passing day, the Internet becomes a bigger part of our lives. Perhaps the most important goal of mine for <a href="../">BestHomepageEver</a> has been to create a page that gives you quick access to all the most important pages on the web.</p>
+      		<h3><img src="../pictures/about/check.png" /> Simplicity</h3><p>As the Internet expands and gets more and more complex, <a href="../">BestHomepageEver</a> has and will adapt to maintain and achieve the simplicity that makes it more and more useful. Simplicity may not have all the bells and whistles of some sites, but it allows you to navigate the site, and the Internet, easier.</p>
+            <h3><img src="../pictures/about/check.png" /> Speed</h3><p>The site is always improving its speed. I am constantly working to minimize load times, to help you (the user) be able to quickly go from your e-mail, to maps, to Google, to reading the news, all within a click each. Overall, my mission here is to allow you to bounce around the Internet through 1 portal, visiting all the sites you wish to as quick as possible.</p>
+            <h3><img src="../pictures/about/check.png" /> No Advertising</h3><p>Last, but not least, one of the reasons why many of you use this site, is there are no pop-ups, no in-your-face advertisements, and no need to stare at products you won't be buying. I have never advertised with banners, and never will. This helps maintain a quicker and cleaner homepage for your enjoyment.</p>
+
+          
+          
+
+
+        </div><!-- /.blog-main -->
+
+  <!--      <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="news-old.html">News Archive</a> <span class="small"></span></li>              
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">              
+              <li><a href="http://twitter.com/besthomepagever">Twitter</a></li> 
+              <li><a href="http://besthomepage.co.uk">BestHomepage UK</a></li>             
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar --> 
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>BestHomepageEver, 2016.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+  </body>
+
+
+
+
